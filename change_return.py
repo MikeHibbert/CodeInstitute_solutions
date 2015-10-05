705d80ffa49e89c33f0d557e169ca0f0da342b88
willemmerson <willemmerson@gmail.com>
2015-10-05 17:13:03 +0100

    fix floating point error

--- change_return.py
+++ change_return.py
@@ -1,10 +1,14 @@
+from decimal import Decimal
+
 coins = [1, .50, .20, .10, .05, .02, .01]
 
 
 def give_change(amount):
     change = []
+    amount = Decimal(str(amount))
     for coin in coins:
+        coin = Decimal(str(coin))
         while coin <= amount:
             amount -= coin
-            change.append(coin)
+            change.append(float(coin))
     return change
