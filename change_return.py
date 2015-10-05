c496dc3433ef2635758b4004a7763752a5756edb
willemmerson <willemmerson@gmail.com>
2015-10-05 17:05:08 +0100

    fix bug for multiple same coins

--- change_return.py
+++ change_return.py
@@ -4,7 +4,7 @@ coins = [100, 50, 20, 10, 5, 2, 1]
 def give_change(amount):
     change = []
     for coin in coins:
-        if coin <= amount:
+        while coin <= amount:
             amount -= coin
             change.append(coin)
     return change
