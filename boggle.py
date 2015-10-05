d92f8862d442d10203a28121dcc8c0b2cac0de64
willemmerson <willemmerson@gmail.com>
2015-10-05 18:24:55 +0100

    initial boggle program

---
+++ boggle.py
@@ -0,0 +1,22 @@
+from random import choice
+from string import ascii_uppercase
+
+
+def get_grid():
+    return {(x, y): choice(ascii_uppercase) for x in range(X) for y in range(Y)}
+
+
+def get_neighbours():
+    neighbours = {}
+
+    for position in grid:
+        x, y = position
+        positions = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x + 1, y),
+                     (x + 1, y + 1), (x, y + 1), (x - 1, y + 1), (x - 1, y)]
+        neighbours[position] = [p for p in positions if 0 <= p[0] < X and 0 <= p[1] < Y]
+    return neighbours
+
+
+size = X, Y = 2, 2
+grid = get_grid()
+neighbours = get_neighbours()
